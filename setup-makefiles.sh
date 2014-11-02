70eec01682451b5eeeab3a30de240035e15cff35
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-11-02 13:07:13 +0100

    Make sure that com.qualcomm.location.apk is not getting included
    Change-Id: I9aa166c73a89b433d294fb84ba952c862c59a3ed

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -179,7 +179,6 @@ EOF
 DEVICE_PACKAGE_OVERLAYS += vendor/$VENDOR/$DEVICE/overlay
 
 PRODUCT_PACKAGES += \\
-    com.qualcomm.location \\
     QuickBoot \\
     qcrilmsgtunnel \\
     shutdownlistener
@@ -245,17 +244,6 @@ ifeq (\$(BOARD_VENDOR),oppo)
 ifeq (\$(TARGET_BOARD_PLATFORM),msm8974)
 
 ifeq (\$(QCPATH),)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
-include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
