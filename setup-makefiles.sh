9c1aeb9da07088c67a6be86b0bfbb364ad880bcc
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-11-25 20:13:43 +0100

    Add back com.qualcomm.location
    Change-Id: Ifcff62d8259ffe5f67069d57c7328ebd2a004fee

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -179,6 +179,7 @@ EOF
 DEVICE_PACKAGE_OVERLAYS += vendor/$VENDOR/$DEVICE/overlay
 
 PRODUCT_PACKAGES += \\
+    com.qualcomm.location \\
     QuickBoot \\
     qcrilmsgtunnel \\
     shutdownlistener
@@ -244,6 +245,17 @@ ifeq (\$(BOARD_VENDOR),oppo)
 ifeq (\$(TARGET_BOARD_PLATFORM),msm8974)
 
 ifeq (\$(QCPATH),)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
